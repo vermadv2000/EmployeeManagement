7a8d893e118870253fdef5e55547db78a82884f6
vermadv2000 <vermadv2000@gmail.com>
2024-10-09 00:08:53 +0530

    Backend Done

---
+++ EmployeeManagement/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {}
+    }
+}
--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,6 +1,40 @@
+using EmployeeManagement.Data;
+using EmployeeManagement.Repo;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseInMemoryDatabase("EmployeeDb")
+); 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyCors", builder =>
+    {
+        builder.WithOrigins("https://localhost:4200");
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
+    });
+});
+
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors("MyCors");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
 
 app.Run();
